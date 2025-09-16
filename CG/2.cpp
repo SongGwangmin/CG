fc15adef8355902e7e3e6ddea4cebf8e8956ab3e
Gwangmin Song <thdalsrhkd@naver.com>
2025-09-16 11:44:08 +0900

    morph 함수 윈도우 크기에 맞게 수정하기

--- CG/2.cpp
+++ CG/2.cpp
@@ -30,11 +30,16 @@ GLdouble Rvalue = 0.0;
 GLdouble Gvalue = 0.0;
 GLdouble Bvalue = 1.0;
 
+int width;
+int height;
+
 ret morph(ret& after, ret& before) {
-    after.x1 = (before.x1 - 250) / 250;
-    after.y1 = (before.y1 - 250) / -250;
-    after.x2 = (before.x2 - 250) / 250;
-    after.y2 = (before.y2 - 250) / -250;
+	int halfwidth = width / 2;
+	int halfheight = height / 2;
+    after.x1 = (before.x1 - halfwidth) / halfwidth;
+    after.y1 = (before.y1 - height) / -height;
+    after.x2 = (before.x2 - halfwidth) / halfwidth;
+    after.y2 = (before.y2 - height) / -height;
 
     //(showingrect[0].x1 - 250) / 250, (showingrect[0].y1 - 250) / -250
 
@@ -67,63 +72,16 @@ bool ptinrect(int x, int y, ret& rect) {
 void Mouse(int button, int state, int x, int y)
 {
     switch (button) {
-    case GLUT_LEFT_BUTTON: {
-        if (state == GLUT_DOWN) { // left click - in: change rectcolor, out: change bgcolor
-            for (int i = 0; i < 4; i++) {
-                if (ptinrect(x, y, showingrect[i])) {
-                    showingrect[i].Rvalue = dis(gen) / 256.0f;
-                    showingrect[i].Gvalue = dis(gen) / 256.0f;
-                    showingrect[i].Bvalue = dis(gen) / 256.0f;
-                    glutPostRedisplay();
-                    break;
-                }
-                else if (ptinrect(x, y, hidingrect[i])) {
-                    GLdouble Rv = dis(gen) / 256.0f;
-                    GLdouble Gv = dis(gen) / 256.0f;
-                    GLdouble Bv = dis(gen) / 256.0f;
+    case GLUT_LEFT_BUTTON:
+    {
 
-                    for (int i = 0; i < 4; i++) {
-                        hidingrect[i].Rvalue = Rv;
-                        hidingrect[i].Gvalue = Gv;
-                        hidingrect[i].Bvalue = Bv;
-                    }
-                    glutPostRedisplay();
-                    break;
-                }
-            }
-        }
     }
-                         break;
-    case GLUT_RIGHT_BUTTON: {
-        if (state == GLUT_DOWN) {
-            for (int i = 0; i < 4; i++) {
-                if (ptinrect(x, y, showingrect[i])) {
-                    if (showingrect[i].level > 0) {
-                        showingrect[i].level--;
-                        showingrect[i].x1 += 25;
-                        showingrect[i].y1 += 25;
-                        showingrect[i].x2 -= 25;
-                        showingrect[i].y2 -= 25;
-                    }
-                    glutPostRedisplay();
-                    break;
-                }
-                else if (ptinrect(x, y, hidingrect[i])) {
-                    if (showingrect[i].level < 3) {
-                        showingrect[i].level++;
-                        showingrect[i].x1 -= 25;
-                        showingrect[i].y1 -= 25;
-                        showingrect[i].x2 += 25;
-                        showingrect[i].y2 += 25;
-                    }
+    break;
+    case GLUT_RIGHT_BUTTON:
+    {
 
-                    glutPostRedisplay();
-                    break;
-                }
-            }
-        }
     }
-                          break;
+    break;
     default:
         break;
 
@@ -161,6 +119,8 @@ void main(int argc, char** argv)
     }
     else
         std::cout << "GLEW Initialized\n";
+    width = glutGet(GLUT_WINDOW_WIDTH);
+    height = glutGet(GLUT_WINDOW_HEIGHT);
     glutDisplayFunc(drawScene);
     glutReshapeFunc(Reshape);
     glutKeyboardFunc(Keyboard);
@@ -174,34 +134,7 @@ GLvoid drawScene()
     glClear(GL_COLOR_BUFFER_BIT);
 
 
-    static int start = 0;
-
-
-
-    if (start == 0) {
-        for (int i = 0; i < 4; i++) {
-            hidingrect[i].Rvalue = 0.5;
-            hidingrect[i].Gvalue = 0.5;
-            hidingrect[i].Bvalue = 0.5;
-
-
-            showingrect[i].Rvalue = dis(gen) / 256.0f;
-            showingrect[i].Gvalue = dis(gen) / 256.0f;
-            showingrect[i].Bvalue = dis(gen) / 256.0f;
-        }
-        start = 1;
-    }
-
-    for (int i{}; i < 4; ++i) {
-        ret drawsupport;
-        drawsupport = morph(drawsupport, hidingrect[i]);
-        glColor3f(drawsupport.Rvalue, drawsupport.Gvalue, drawsupport.Bvalue);
-        glRectf(drawsupport.x1, drawsupport.y1, drawsupport.x2, drawsupport.y2);
-
-        drawsupport = morph(drawsupport, showingrect[i]);
-        glColor3f(drawsupport.Rvalue, drawsupport.Gvalue, drawsupport.Bvalue);
-        glRectf(drawsupport.x1, drawsupport.y1, drawsupport.x2, drawsupport.y2);
-    }
+    
     // 그리기 부분 구현: 그리기 관련 부분이 여기에포함된다.
     glutSwapBuffers();
 }
@@ -229,59 +162,6 @@ GLvoid Reshape(int w, int h)
 
 void Keyboard(unsigned char key, int x, int y) {
     switch (key) {
-    case 'c': // 청록색 (초록+파랑)
-        timerRunning = false;
-        Rvalue = 0.0;
-        Gvalue = 1.0;
-        Bvalue = 1.0;
-        break;
-
-    case 'm': // 자홍색 (빨강+파랑)
-        timerRunning = false;
-        Rvalue = 1.0;
-        Gvalue = 0.0;
-        Bvalue = 1.0;
-        break;
-
-    case 'y': // 노랑색 (빨강+초록)
-        timerRunning = false;
-        Rvalue = 1.0;
-        Gvalue = 1.0;
-        Bvalue = 0.0;
-        break;
-
-    case 'a': // 랜덤색
-        Rvalue = dis(gen) / 256.0f;
-        Gvalue = dis(gen) / 256.0f;
-        Bvalue = dis(gen) / 256.0f;
-        timerRunning = false;
-        break;
-
-    case 'w': // 백색
-        timerRunning = false;
-        Rvalue = 1.0;
-        Gvalue = 1.0;
-        Bvalue = 1.0;
-        break;
-
-    case 'k': // 검정색
-        timerRunning = false;
-        Rvalue = 0.0;
-        Gvalue = 0.0;
-        Bvalue = 0.0;
-        break;
-
-    case 't': // 타이머 시작
-        if (!timerRunning) {
-            timerRunning = true;
-            glutTimerFunc(100, TimerFunc, 0); // 1초마다 랜덤 색 갱신
-        }
-        break;
-
-    case 's': // 타이머 종료
-        timerRunning = false;
-        break;
-
     case 'q': // 프로그램 종료
         glutLeaveMainLoop();
         break;
